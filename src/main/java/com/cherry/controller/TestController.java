406b7d5e9dd58101155dcdd710704a0bef6fb075
Cheronaldo <601585410@qq.com>
2017-11-05 16:29:02 +0800

    2017.11.05 16:28

---
+++ src/main/java/com/cherry/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.cherry.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2017/11/05.
+ */
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/run")
+    public String test(){
+        return "test";
+    }
+
+
+}
