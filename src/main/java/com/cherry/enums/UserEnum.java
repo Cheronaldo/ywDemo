102aed47dd9cade9830805c6e0b07c2f56e9a0a5
Cheronaldo <601585410@qq.com>
2017-11-08 10:31:41 +0800

    2017.11.08 10:31

---
+++ src/main/java/com/cherry/enums/UserEnum.java
@@ -0,0 +1,36 @@
+package com.cherry.enums;
+
+/**
+ * 用户类 操作枚举
+ * Created by Administrator on 2017/11/08.
+ */
+public enum UserEnum {
+    USER_ALREADY_EXIST(1,"用户已存在"),
+    USER_VALID(2,"用户名符合要求"),
+    USER_INFORMATION_ERROR(3,"用户信息错误"),
+    USER_REGISTER_SUCCESS(4,"注册成功"),
+    USER_LOGIN_FAIL(5,"登录失败"),
+    USER_LOGIN_SUCCESS(6,"登录成功"),
+    USER_GET_FAIL(7,"用户信息获取失败"),
+    USER_GET_SUCCESS(8,"用户信息获取成功"),
+
+    ;
+
+
+    private Integer code;
+
+    private String message;
+
+    UserEnum(Integer code, String message){
+        this.code = code;
+        this.message = message;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
